4eec2f5d7fc9fe4f6f23f37383d19f6bee4f94d9
scardenasb <84429399+scardenasb@users.noreply.github.com>
2022-06-28 03:16:34 -0400

    update

--- installer.py
+++ installer.py
@@ -1,14 +1,18 @@
 import os
 
 
-os.system('sudo apt update')
+os.system("sudo apt update")
 
-os.system('sudo apt install apt-transport-https curl')
+os.system("sudo apt install apt-transport-https curl")
 
-os.system('sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg')
+os.system(
+    "sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg"
+)
 
-os.system('echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list')
+os.system(
+    'echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list'
+)
 
-os.system('sudo apt update')
+os.system("sudo apt update")
 
-os.system('sudo apt install brave-browser')
+os.system("sudo apt install brave-browser")
